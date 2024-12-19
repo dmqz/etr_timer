e6e7e3445e9f917a85de1b1dbeabef73de348d5a
dmqz <103788024+dmqz@users.noreply.github.com>
2024-12-19 14:58:22 -0800

    Update event.py

--- event.py
+++ event.py
@@ -7,7 +7,7 @@ GPIO.setwarnings(False) #Ignore warning for now
 GPIO.setmode(GPIO.BOARD) #Use physical pin numbering
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(18,GPIO.RISING,callback=button_callback) #Setup event on pin 10 rising edge
+GPIO.add_event_detect(18, GPIO.RISING, callback=button_callback) #Setup event on pin 10 rising edge
 
 message = input("Press enter to quit\n\n") #Run until someone presses enter
 
