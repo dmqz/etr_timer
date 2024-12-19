dc4901ec58521ac5b1b830fa153c8557d1cfc6f6
dmqz <103788024+dmqz@users.noreply.github.com>
2024-12-19 15:04:55 -0800

    Update event.py

--- event.py
+++ event.py
@@ -1,14 +1,14 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
 def button_callback(channel):
     print("Button was pushed!")
 
-GPIO.setwarnings(False) #Ignore warning for now
-GPIO.setmode(GPIO.BOARD) #Use physical pin numbering
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set GPIO pin 17 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(18, GPIO.RISING, callback=button_callback) #Setup event on pin 10 rising edge
+GPIO.add_event_detect(17, GPIO.RISING, callback=button_callback)  # Setup event on GPIO pin 17 rising edge
 
-message = input("Press enter to quit\n\n") #Run until someone presses enter
+message = input("Press enter to quit\n\n")  # Run until someone presses enter
 
-GPIO.cleanup() # Clean up
+GPIO.cleanup()  # Clean up
